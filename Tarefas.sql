91da0f3d3b4d72c29a9211e6f925a079f28f6d3a
GuilhermeFernandesCruz <gui.fernandes388@gmail.com>
2025-03-06 17:24:21 -0300

    Update Tarefas.sql

--- Tarefas.sql
+++ Tarefas.sql
@@ -8,7 +8,7 @@ CREATE TABLE categoria (
 CREATE TABLE usuario (
     idusuario INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(90) NOT NULL,
-    email VARCHAR(30) NOT NULL
+    email VARCHAR(30) NOT NULL UNIQUE -- Garantindo que o email seja único
 );
 
 -- Tabela de Tarefas (uma tarefa pertence a um usuário e a uma categoria)
@@ -19,5 +19,7 @@ CREATE TABLE tarefa (
     data_criacao DATE NOT NULL,
     idcategoria INT,  -- Relacionamento com a categoria
     idusuario INT,    -- Relacionamento com o usuário
-    FOREIGN KEY (idcategoria) REFERENCES categoria(idcategoria)
+    FOREIGN KEY (idcategoria) REFERENCES categoria(idcategoria),
     FOREIGN KEY (idusuario) REFERENCES usuario(idusuario)
+);
+
